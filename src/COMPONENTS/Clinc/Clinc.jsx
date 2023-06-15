36768d5344ed9321f6dc291c253a7a865d896f83
SaraMagdy <sm9976928@gmail.com>
2023-06-15 17:28:23 +0300

    clinic-changes

--- src/COMPONENTS/Clinc/Clinc.jsx
+++ src/COMPONENTS/Clinc/Clinc.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./Clinc.css"
+import "./Clinc.css";
 // import Navbar from "../NavBar/NavBar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
@@ -41,56 +41,86 @@ const Clinics = () => {
       </div>
       <h2 className="clinic-header">Clinics</h2>
       <div className="clinic-parentCard container">
-        {clinics.map((clinic) => (
-          <div
-            key={clinic.id}
-            className="clinic-card"
-            onClick={() => handleCardClick(clinic)}
-          >
-            <FontAwesomeIcon
-              icon={faCircleInfo}
-              size="xl"
-              color="#ff642e"
-              style={{ position: "absolute", right: "12" , top : "10px" }}
-              onClick={() => handleCardClick(clinic)}
+        <div className="row">
+          {clinics.map((clinic) => (
+            <div className="col-sm-12 col-md-6 col-lg-4 px-3">
+              <div
+                key={clinic.id}
+                className="clinic-card "
+                onClick={() => handleCardClick(clinic)}
+              >
+                <div className="clinic-card-header">
+                  <img src={clinic.img} alt="img" width="400px"></img>
+                </div>
+                <div className="clinic-card-body">
+                  <h3 className="ClinicName">{clinic.name}</h3>
+                  <p>
+                    {" "}
+                    <FontAwesomeIcon
+                      icon={faLocationDot}
+                      color="#ff642e"
+                    />{" "}
+                    &nbsp; &nbsp;
+                    {clinic.address}
+                  </p>
 
-            />
+                  <button
+                    className="clinic-btn "
+                    onClick={() => handleCardClick(clinic)}
+                  >
+                    Details
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
 
-            <div className="clinic-card-header">
-              <img src={clinic.img} alt="img" width="400px"></img>
+          {isPopupOpen && (
+            <div className="clinic-popup">
+              <div className="clinic-popup-content">
+                <button className="clinic-close-btn" onClick={handlePopupClose}>
+                  X
+                </button>
+                <h3 className="popUp-ClinicName">{selectedClinic.name}</h3>
+                <p>
+                  <FontAwesomeIcon icon={faPhone} color="#ff642e" />
+                  &nbsp; &nbsp;
+                  {selectedClinic.phone}
+                </p>
+                <p>
+                  {" "}
+                  <FontAwesomeIcon icon={faLocationDot} color="#ff642e" />{" "}
+                  &nbsp; &nbsp;
+                  {selectedClinic.fullAddress}
+                </p>
+                <p>
+                  <FontAwesomeIcon icon={faCalendarCheck} color="#ff642e" />{" "}
+                  Avilable Appointements :{selectedClinic.availableAppointments}
+                </p>
+                <div>
+                  <iframe
+                    width="90%"
+                    height="200"
+                    title="location"
+                    src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=20%20El%20Gezira%20Street,%20Zamalek,%20Cairo+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+                  >
+                    <a href="https://www.maps.ie/distance-area-calculator.html">
+                      measure area map
+                    </a>
+                  </iframe>
+                </div>
+              </div>
             </div>
-            <div className="clinic-card-body">
-              <h3 className="ClinicName">{clinic.name}</h3>
-            </div>
-          </div>
-        ))}
-        {isPopupOpen && (
-          <div className="clinic-popup">
-            <div className="clinic-popup-content">
-              <button className="clinic-close-btn" onClick={handlePopupClose}>
-                X
-              </button>
-              <h3 className="ClinicName">{selectedClinic.name}</h3>
-              <hp>
-                <FontAwesomeIcon icon={faPhone} color="#ff642e" />
-                &nbsp; &nbsp;
-                {selectedClinic.phone}
-              </hp>
-              <p>
-                {" "}
-                <FontAwesomeIcon icon={faLocationDot} color="#ff642e" /> &nbsp;
-                &nbsp;
-                {selectedClinic.address}
-              </p>
-              <p>
-                <FontAwesomeIcon icon={faCalendarCheck} color="#ff642e" />{" "}
-                Avilable Appointements :<br></br>
-                {selectedClinic.availableAppointments}
-              </p>
-            </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
+      {/* <div className="container">
+        <div className="row">
+          <div className="col-md-4">3</div>
+          <div className="col-md-4">3</div>
+          <div className="col-md-4">3</div>
+        </div>
+      </div> */}
     </div>
   );
 };
